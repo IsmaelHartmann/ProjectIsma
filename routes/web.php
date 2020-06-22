96a0b6e99e427da2c88a0725d0aa6f95e66dbbe5
IsmaelHartmann <ismael.hartmann@hotmail.com>
2020-06-22 10:36:08 -0300

    add route /

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resources([
+    'movimentacoes' => 'MovController'
+]);
 
 
 Route::resources([
-    'movimentacoes' => 'MovController'
+    '/' => 'MovController'
 ]);
